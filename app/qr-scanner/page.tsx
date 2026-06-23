b4ff4e8076650d05ab944aec312668568f5bd6f5
Gobinda Subedi <fairanalyst93@gmail.com>
2026-06-23 15:41:19 +0545

    Switch to otpauth

--- app/qr-scanner/page.tsx
+++ app/qr-scanner/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { authenticator } from 'otplib'
+import * as OTPAuth from 'otpauth'
 
 export default function Test() {
   const [code, setCode] = useState('loading...')
 
   useEffect(() => {
     try {
-      const c = authenticator.generate('ZBLWUEP2WDWCOJR7BZ3Z6NWLAZHDFNHV')
-      setCode(c)
+      const totp = new OTPAuth.TOTP({
+        secret: 'ZBLWUEP2WDWCOJR7BZ3Z6NWLAZHDFNHV'
+      })
+      setCode(totp.generate())
     } catch (e:any) {
       setCode('ERROR: ' + e.message)
     }
@@ -16,9 +18,8 @@ export default function Test() {
 
   return (
     <main className="p-10">
-      <h1 className="text-2xl">OTPLIB Test</h1>
+      <h1 className="text-2xl">OTPAUTH Test</h1>
       <div className="text-5xl font-mono mt-4">{code}</div>
-      <p className="mt-4 text-sm">If you see 6 digits, otplib works on Vercel.</p>
     </main>
   )
 }
