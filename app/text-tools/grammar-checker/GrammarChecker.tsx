db5ee364b242f4df8f7eae03096e05860f5e98c7
Gobinda Subedi <fairanalyst93@gmail.com>
2026-06-26 15:27:32 +0545

    fix grammar checker component

---
+++ app/text-tools/grammar-checker/GrammarChecker.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function GrammarChecker(){
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Grammar Checker</h1>
+      <p>Coming soon</p>
+    </main>
+  )
+}
